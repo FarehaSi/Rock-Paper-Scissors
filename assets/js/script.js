586694a191212207384b2969bd829c80c45a42b4
FarehaSi <fareha.s@live.com>
2023-02-03 12:03:25 +0000

    Add core structure to game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,19 @@ const WEAPONS = [
       winsOver: "rock",
     },
   ];
+
+  function chooseWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        // Draw
+    }
+
+    // playerChoice - rock; computerChoice - paper
+    const playerChoiceConfig = WEAPONS.find
+    (eachWeapon => eachWeapon.id === playerChoice);
+
+    if(playerChoiceConfig.winsOver === computerChoice) {
+        // Player wins
+    } else {
+        // Computer wins
+    }
+}
