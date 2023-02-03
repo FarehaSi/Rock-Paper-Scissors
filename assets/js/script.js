f16947f8f46a3f098355c966aec6576ffe604e16
FarehaSi <fareha.s@live.com>
2023-02-03 11:48:49 +0000

    Define 'WEAPONS' variable in JS

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const WEAPONS = [
+    {
+      id: "rock",
+      name: "Rock",
+      imgSrc: "rock.png",
+      winsOver: "scissors",
+    },
+    {
+      id: "scissors",
+      name: "Scissors",
+      imgSrc: "scissors.png",
+      winsOver: "paper",
+    },
+    {
+      id: "paper",
+      name: "Paper",
+      imgSrc: "paper.png",
+      winsOver: "rock",
+    },
+  ];
